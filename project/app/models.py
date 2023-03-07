8b6986df19cd2182dcf9e6c1bb44a16cc6a4cb70
Tony Redekop <tony.redekop@outlook.com>
2023-03-07 13:33:00 -0800

    setup: create basic models, register app in settings.py

--- project/app/models.py
+++ project/app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+# Create models
+# Note: Django automatically creates auto-incrementing primary key
+class ManufacturingProcess(models.Model):
+  name = models.CharField(max_length=255)
+  description = models.TextField()
+
+class Operation(models.Model):
+  name = models.CharField(max_length=255)
+  description = models.TextField()
+  cycle_time = models.DurationField() 
+  process = models.ForeignKey(
+    ManufacturingProcess,
+    on_delete=models.CASCADE,
+  )
