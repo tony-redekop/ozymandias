8083267e85fc3b4bb771224566056b79f640682c
Tony Redekop <tony.redekop@outlook.com>
2023-03-09 19:21:42 -0800

    tests: create unit test for database models

--- project/tests/tests.py
+++ project/tests/tests.py
@@ -1,12 +1,34 @@
+from datetime import timedelta
 from django.test import TestCase
+
 from app.models import ManufacturingProcess
 from app.models import Operation
 
-class ManufacturingProcessTestCase(TestCase):
-  def setup(self):
-    ManufacturingProcess.objects.create(name="Recieving Inspection", description="Visual inspection for obvious damage")
-  
-  # Define test methods 
-  # Note: Test method names must start with 'test'
-  def test1(self):
-    print("test1 passed")
+# Note: setUp() is run before each test method is run.
+# Django flushes test database after each test method completes.
+# PostgreSQL doesn't reset auto-incrementing primary key after a flush.
+# Test method names must start with 'test' to be found by Django.
+class MainTestCase(TestCase):
+  def setUp(self):
+    # Create model instances and save
+    self.manufacturing_process = ManufacturingProcess(
+      name="", 
+      description=""
+    )
+    self.manufacturing_process.save()
+    self.operation = Operation(
+      name="", 
+      description="",
+      cycle_time=timedelta(seconds=3600),
+      process_id=self.manufacturing_process.id  # Foreign key
+    )
+    self.operation.save()
+
+  def test_models(self):
+    try:
+      ManufacturingProcess.objects.get(id=1)
+      Operation.objects.get(id=1)
+    except ManufacturingProcess.DoesNotExist:
+      raise ValueError("Invalid primary key for ManufacturingProcess")
+    except Operation.DoesNotExist:
+      raise ValueError("Invalid primary key for Operation")
