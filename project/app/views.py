8ed548a5308f0747ceb07161b632daf11d7410e5
Tony Redekop <tony.redekop@outlook.com>
2023-03-11 11:08:05 -0800

    feature: create views

--- project/app/views.py
+++ project/app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import permissions
+from app.models import ManufacturingProcess
+from app.models import Operation
+from app.serializers import ManufacturingProcessSerializer
+from app.serializers import OperationSerializer
 
-# Create your views here.
+class ManufacturingProcessViewSet(viewsets.ModelViewSet):
+  # API endpoint that allows manufacturing processes to be viewed or edited
+  queryset = ManufacturingProcess.objects.all()
+  serializer_class = ManufacturingProcessSerializer
+  permission_classes = [permissions.IsAuthenticated]
+
+class OperationViewSet(viewsets.ModelViewSet):
+  # API endpoint that allows manufacturing processes to be viewed or edited
+  queryset = Operation.objects.all()
+  serializer_class = OperationSerializer
+  permission_classes = [permissions.IsAuthenticated]
