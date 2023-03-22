922cdb4e5014d05905d77becb81f0c82a9b11064
Tony Redekop <tony.redekop@outlook.com>
2023-03-22 14:59:33 -0700

    fix: change Model field-specific arguments to 'blank=True' to allow empty field values during the validation; make resulting migrations

--- project/app/models.py
+++ project/app/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
-# Create models
-# Note: Django automatically creates auto-incrementing primary key
+# Note: Django automatically creates auto-incrementing primary key field 'id', but this can be overriden
+# The 'blank' argument is validation-related.  If a field has blank=False, the field will be required.
 class ManufacturingProcess(models.Model):
-  name = models.CharField(max_length=255)
-  description = models.TextField()
+  name = models.CharField(blank=True, max_length=255)
+  description = models.TextField(blank=True)
 
 class Operation(models.Model):
   name = models.CharField(max_length=255)
